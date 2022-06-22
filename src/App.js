68992275d7c2ae05d9998277ad50a33177415e14
Carlos Del Valle <cadelvalledieguez@gmail.com>
2022-06-22 15:41:46 +1000

    Before-add-class-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
 
-  const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-    ]);
-  };
-  
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
